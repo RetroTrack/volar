34721de75aea5a3e9803e35ebf3d77fd95741005
Retro <96375203+RetroTrack@users.noreply.github.com>
2022-07-16 13:46:56 +0200

    lot of changes
    online/offline status + latency, and nicer look for offline servers

--- js/status-survival.js
+++ js/status-survival.js
@@ -4,14 +4,15 @@ var url = "https://api.minetools.eu/ping/play.volar.ga"; //insert  server here
 $.getJSON(url, function(r) {
   //data is the JSON string
   if (r.error) {
-    $('#rest-survival').html('<br><br><b><br><br><font color= "#E74C3C">Server Offline</b><br><br>');
+    $('#rest-survival').html('<br><br><b><br><font color= "#E74C3C">Server Offline</b><br><br>');
     return false;
   }
+  var latency = Math.round(r.latency);
   var pl = '';
   if (r.players.sample.length > 0) {
     pl = '<br>OP: ' + r.players.sample[0].name;
   }
-  $('#rest-survival').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><br>');
+  $('#rest-survival').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><font color= "#333"><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><b>Latency:</b> ' + latency + 'ms' + '<br><font color= "#28B463">Server Online' + '<br><br>');
   $('#favicon-survival').attr('src', r.favicon);
 
 });

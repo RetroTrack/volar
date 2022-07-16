16b979c9a9f871d264a54576d67d706a9de1eec5
Retro <96375203+RetroTrack@users.noreply.github.com>
2022-07-16 12:59:18 +0200

    changes to server status

--- js/status-create.js
+++ js/status-create.js
@@ -4,14 +4,14 @@ var url = "https://api.minetools.eu/ping/create.volar.ga"; //insert  server here
 $.getJSON(url, function(r) {
   //data is the JSON string
   if (r.error) {
-    $('#rest-create').html('Server Offline');
+    $('#rest-create').html('<br><br><b><br><br><font color= "#E74C3C">Server Offline</b><br><br>');
     return false;
   }
   var pl = '';
   if (r.players.sample.length > 0) {
     pl = '<br>OP: ' + r.players.sample[0].name;
   }
-  $('#rest-create').html(r.description.replace(/§(.+?)/gi, '') + '<br><br><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max );
+  $('#rest-create').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><br>');
   $('#favicon-create').attr('src', r.favicon);
 
 });
--- js/status-survival.js
+++ js/status-survival.js
@@ -4,14 +4,14 @@ var url = "https://api.minetools.eu/ping/play.volar.ga"; //insert  server here
 $.getJSON(url, function(r) {
   //data is the JSON string
   if (r.error) {
-    $('#rest-survival').html('Server Offline');
+    $('#rest-survival').html('<br><br><b><br><br><font color= "#E74C3C">Server Offline</b><br><br>);
     return false;
   }
   var pl = '';
   if (r.players.sample.length > 0) {
     pl = '<br>OP: ' + r.players.sample[0].name;
   }
-  $('#rest-survival').html(r.description.replace(/§(.+?)/gi, '') + '<br><br><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max );
+  $('#rest-survival').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><br>');
   $('#favicon-survival').attr('src', r.favicon);
 
 });

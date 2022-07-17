9cfec0f7509f8b26dac370ed2b8675e6c36852b7
Retro <96375203+RetroTrack@users.noreply.github.com>
2022-07-17 14:57:15 +0200

    server version go yes
    yes

--- js/status-create.js
+++ js/status-create.js
@@ -4,14 +4,14 @@ var url = "https://api.minetools.eu/ping/create.volar.ga"; //insert  server here
 $.getJSON(url, function(r) {
   //data is the JSON string
   if (r.error) {
-    $('#rest-create').html('<br>This is an example text that shows when the server is offline.<br><br><font color= "#333"><b>Players Online:</b>?<b>/</b>?<br> <b>Latency:</b> ?ms<br><font color= "#E74C3C">Server Offline<br><br>');
+    $('#rest-create').html('<br>This is an example text that shows when the server is offline.<br><br><font color= "#333"><b>Players Online:</b>?<b>/</b>?<br> <b>Latency:</b> ?ms<br><b>Version:</b> ?<br><font color= "#E74C3C">Server Offline<br><br>');
     return false;
   }
   var pl = '';
   if (r.players.sample.length > 0) {
     pl = '<br>OP: ' + r.players.sample[0].name;
   }
-  $('#rest-create').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><font color= "#333"><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><b>Latency:</b> ' + latency + 'ms' + '<br><font color= "#28B463">Server Online' + '<br><br>');
+  $('#rest-create').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><font color= "#333"><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><b>Latency:</b> ' + latency + 'ms' + '<br><b>Version:</b> '+ r.version.name + '<br><font color= "#28B463">Server Online' + '<br><br>');
   $('#favicon-create').attr('src', r.favicon);
 
 });
--- js/status-survival.js
+++ js/status-survival.js
@@ -4,7 +4,7 @@ var url = "https://api.minetools.eu/ping/play.volar.ga"; //insert  server here
 $.getJSON(url, function(r) {
   //data is the JSON string
   if (r.error) {
-    $('#rest-survival').html('<br><br><b><br><font color= "#E74C3C">Server Offline</b><br><br>');
+    $('#rest-survival').html('<br>This is an example text that shows when the server is offline.<br><br><font color= "#333"><b>Players Online:</b>?<b>/</b>?<br> <b>Latency:</b> ?ms<br><b>Version:</b> ?<br><font color= "#E74C3C">Server Offline<br><br>');
     return false;
   }
   var latency = Math.round(r.latency);
@@ -12,7 +12,7 @@ $.getJSON(url, function(r) {
   if (r.players.sample.length > 0) {
     pl = '<br>OP: ' + r.players.sample[0].name;
   }
-  $('#rest-survival').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><font color= "#333"><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><b>Latency:</b> ' + latency + 'ms' + '<br><font color= "#28B463">Server Online' + '<br><br>');
+  $('#rest-survival').html('<br>' + r.description.replace(/§(.+?)/gi, '') + '<br><br><font color= "#333"><b>Players Online:</b> ' + r.players.online + '<b>/</b>'+ r.players.max + '<br><b>Latency:</b> ' + latency + 'ms' + '<br><b>Version:</b> '+ r.version.name + '<br><font color= "#28B463">Server Online' + '<br><br>');
   $('#favicon-survival').attr('src', r.favicon);
 
 });
